1498c6d7a1747fe545a4366a2b594dd2f1290bfb
Gustorn <gustorn@gmail.com>
2017-06-12 21:09:47 +0200

    Scrollbar work

---
+++ src/components/scrollbars/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactScrollbars, { ScrollbarProps } from "react-custom-scrollbars";
+
+export default class Scrollbars extends React.Component<OwnProps, {}> {
+	render() {
+		const { children, ...rest } = this.props;
+		return (
+			<ReactScrollbars ref={this.setScrollRef} {...rest}>
+				{children}
+			</ReactScrollbars>
+		);
+	}
+
+	setScrollRef = (element: any) => {
+		const { scrollRef } = this.props;
+		scrollRef(element.view);
+	}
+}
+
+interface OwnProps extends ScrollbarProps {
+	scrollRef: (element: any) => void;
+}
